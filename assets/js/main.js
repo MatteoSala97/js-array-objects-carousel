3ddb5242f4ea762bd20d3be3b5135286427240d2
MatteoSala97 <matteo111997@gmail.com>
2024-01-23 14:56:34 +0100

    fix: added array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,83 @@
+// Array 1 - Monkey D. Luffy
+const array1 = {
+    image: "./assets/img/luffy.jpg",
+    crew: "Straw Hat",
+    role: "Captain",
+    devilFruit: "Gomu Gomu no Mi"
+}
+
+// Array 2 - Roronoa Zoro
+const array2 = {
+    image: "./assets/img/zoro.jpg",
+    crew: "Straw Hat",
+    role: "Swordsman",
+    devilFruit: ""
+}
+
+// Array 3 - Nami
+const array3 = {
+    image: "./assets/img/nami.jpg",
+    crew: "Straw Hat",
+    role: "Navigator",
+    devilFruit: ""
+}
+
+// Array 4 - Usopp
+const array4 = {
+    image: "./assets/img/usopp.jpg",
+    crew: "Straw Hat",
+    role: "Sniper",
+    devilFruit: ""
+}
+
+// Array 5 - Sanji
+const array5 = {
+    image: "./assets/img/sanji.jpg",
+    crew: "Straw Hat",
+    role: "Chef",
+    devilFruit: ""
+}
+
+// Array 6 - Tony Tony Chopper
+const array6 = {
+    image: "./assets/img/chopper.jpg",
+    crew: "Straw Hat",
+    role: "Doctor",
+    devilFruit: "Hito Hito no Mi, Model: Human"
+}
+
+// Array 7 - Nico Robin
+const array7 = {
+    image: "./assets/img/robin.jpg",
+    crew: "Straw Hat",
+    role: "Archaeologist",
+    devilFruit: "Hana Hana no Mi"
+}
+
+// Array 8 - Franky
+const array8 = {
+            image: "./assets/img/franky.jpg",
+            crew: "Straw Hat",
+            role: "Shipwright",
+            devilFruit: ""
+}
+
+// Array 9 - Brook
+const array9 = {
+        image: "./assets/img/brook.jpg",
+        crew: "Straw Hat",
+        role: "Musician",
+        devilFruit: "Yomi Yomi no Mi"
+}
+
+// Array 10 - Jinbei
+const array10 = {
+    image: "./assets/img/jinbe.jpg",
+    crew: "Straw Hat",
+    role: "Helmsman",
+    devilFruit: ""
+}
+
 //declaring current index 1 to start with first image
 let currentIndex = 1
 showSlides(currentIndex)
@@ -24,7 +104,7 @@ function stopSlideshow() {
     clearInterval(slideInterval)
 }
 
-  // Function to initialize the slideshow
+// Function to initialize the slideshow
 function initializeSlideshow() {
     showSlides(currentIndex) 
     startSlideshow() 
